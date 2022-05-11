b3c8ebd6ce0869aad347b5f40656abc181ecc136
Rafał Opiłowski <rafalopilowski1@outlook.com>
2022-05-11 21:03:04 +0200

    Quick formatting

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "cli/commands/help.hpp"
 #include "cli/flags.hpp"
 
-auto main(int char_count, char* argument_vector[]) -> int {
+auto main(int char_count, char *argument_vector[]) -> int {
     switch (char_count) {
         case 1:
             std::cerr << "Not enough arguments" << "\n\n";
@@ -12,7 +12,7 @@ auto main(int char_count, char* argument_vector[]) -> int {
         case 3:
         case 4:
         case 5:
-            cli::run(char_count,argument_vector);
+            cli::run(char_count, argument_vector);
             break;
         default:
             std::cerr << "Too many arguments" << "\n\n";
