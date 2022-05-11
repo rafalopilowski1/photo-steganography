42c9f1825c8741cf28da8d5805a61e8c671562d4
Rafał Opiłowski <rafalopilowski1@outlook.com>
2022-05-11 21:55:13 +0200

    These are not bytes, but bites...

--- cli/commands/check.cpp
+++ cli/commands/check.cpp
@@ -7,13 +7,15 @@
 namespace cli {
     auto check(const std::string &filePath, const std::string &message) -> void {
         auto ppmInputFile = image::formats::Ppm(filePath);
-        std::vector<image::Pixel> & pixelVector = ppmInputFile.get_pixel_vector();
+        std::vector<image::Pixel> &pixelVector = ppmInputFile.get_pixel_vector();
         auto messageLenBits = message.size() * 8;
         auto imageSizePotencial = (pixelVector.size() * 3);
         if (messageLenBits <= imageSizePotencial) {
-            std::cout << "Given message (" << messageLenBits << ") bytes can be written into this image (" << imageSizePotencial << ") bytes." << '\n';
+            std::cout << "Given message (" << messageLenBits << " bits) can be written into this image ("
+                      << imageSizePotencial << " bits)" << '\n';
         } else {
-            std::cout << "Given message (" << messageLenBits << ") bytes can't be written into this image (" << imageSizePotencial << ") bytes." << '\n';
+            std::cout << "Given message (" << messageLenBits << " bits) can't be written into this image ("
+                      << imageSizePotencial << " bits)" << '\n';
         }
     }
 }
