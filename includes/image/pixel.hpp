2bd13dff7628334c705161004f5fc28d46cace5f
Rafał Opiłowski <rafalopilowski1@outlook.com>
2022-06-14 23:05:51 +0200

    FIX: wrong `std::size_t`, replace with `uint8_t`

--- includes/image/pixel.hpp
+++ includes/image/pixel.hpp
@@ -16,7 +16,7 @@ namespace image {
         std::bitset<8> green;
         std::bitset<8> blue;
 
-        Pixel(unsigned int x, unsigned int y, std::size_t red, std::size_t green, std::size_t blue) :
+        Pixel(unsigned int x, unsigned int y, uint8_t red, uint8_t green, uint8_t blue) :
                 x(x), y(y), red(red), green(green), blue(blue) {};
 
         Pixel(unsigned int x, unsigned int y, std::bitset<8> red, std::bitset<8> green, std::bitset<8> blue) :
